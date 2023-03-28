aea511245f6f05c4395fd9bac38bb552e3fd163a
svalencia014 <sv.samuel.valencia014@gmail.com>
2023-03-27 21:46:29 -0400

    update

---
+++ readme.md
@@ -0,0 +1,6 @@
+# vATC Helper
+## Route Api
+Types:
+- 0: One Route, `route` is the route
+- 1: 2 Routes, `route0` is east ops, `route1` is west ops
+- 2: 2 Routes, `route0` is south ops, `route1` is north ops
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,35 @@
 let button = document.querySelector('button');
+let data = document.getElementById('data');
 
-button.addEventListener('click', () => {
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        if (response.status == 404) {
+            return "Not Found";
+        } else {
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        if (response.status == 404) {
+            return "Not Found";
+        }
+        console.error(error);
+    }
+}
+
+button.addEventListener('click', async () => {
     let dep = document.getElementById('departure').value;
     let arr = document.getElementById('arrival').value;
-    console.log(dep, arr);
+    let url = "https://api.svalencia.cf/route/" + dep + arr;
+    let route = await getData(url);
+    if (route.type == 1) {
+        data.innerText = `East Ops: ${route.route0}\n\nWest Ops: ${route.route1}`;
+    } else if (route.type == 2) {
+        data.innerText = `North Ops: ${route.route1}\n\nSouth Ops: ${route.route0}`;
+    } else if (route.type == 0) {
+        data.innerText = route.route;
+    } else {
+        data.innerText = route;
+    }
 });
